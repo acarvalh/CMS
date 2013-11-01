d2f6a875f07af576f5917054c89b3bbaf5458f2b
acarvalh <alexandracaan@gmail.com>
2013-11-01 21:08:27 +0100

    BKG parametrization, 2 parameter leveled exponential

---
+++ aa_bb/Limits_codes/vOctober13/4bodyFit/models_mtot_bkg.rs
@@ -0,0 +1,8 @@
+mtot[320,1200];
+
+CMS_mtot_bkg_8TeV_slope1_cat0[10., -1000, 1000.0];
+CMS_mtot_bkg_8TeV_slope2_cat0[10., -1000, 1000.0];
+
+
+CMS_mtot_bkg_8TeV_slope1_cat1[10., -1000, 1000.0];
+CMS_mtot_bkg_8TeV_slope2_cat1[10., -1000, 1000.0];
